19e20810df5a1f33a88bec16e47a0524d7a9e976
Adam Tyson <ultrascapes@icloud.com>
2021-01-08 14:41:30 -0800

    Custom components
    Additional template parts in a new custom folder, where project specific layouts and design can be stored.

--- header-home.php
+++ header-home.php
@@ -7,40 +7,5 @@
 </head>
 <body <?php body_class(); ?>>
     <?php get_template_part( '/template-parts/content', 'icons' ); ?>
-    <div class="sans-pg-fold-r2 sans-100vh">
-        <div class="sans-header-image"
-            <?php if ( get_header_image() ) : ?>
-                style="background-image: url('<?php header_image(); ?>');"
-            <?php endif; ?>
-            >
-            <div class="sans-background-tint">
-                <header id="masthead" role="banner">
-                    <?php get_template_part( '/template-parts/header', 'nav' ); ?>
-                    <div class="sans-top-brand-home">
-                        <div class="sans-cont">
-                            <?php
-                                if ( function_exists( 'the_custom_logo' ) ) {
-                                            the_custom_logo();
-                                }
-                            ?>
-                        </div>
-                    </div>
-                </header>
-                <div id="home-header-r3" class="sans-txt-ctr">
-                    <div class="arch-header-home" style="background-image:url(<?php echo(get_stylesheet_directory_uri()); ?>/img/arch-1.svg);">
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="home-site-intro">
-            <div class="site-name sans-cont"><?php bloginfo( 'name' ); ?>
-            </div>
-            <div class="site-desc sans-cont"><?php bloginfo( 'description' ); ?>
-            </div>   
-            <a id="learn-more" href="#home-main">
-                Learn More<br>
-                <svg class="sans-tri-dwn"><use href="#sans-tri-down"></use></svg>
-            </a>    
-        </div>
-    </div>
+    <?php get_template_part( '/custom/header', 'home' ); ?>
 
--- header.php
+++ header.php
@@ -7,35 +7,5 @@
 </head>
 <body <?php body_class(); ?>>
     <?php get_template_part( '/template-parts/content', 'icons' ); ?>
-
-    <div class="sans-pg-fold-r2">
-        <div class="sans-header-image"   
-            <?php if( has_post_thumbnail() ): ?>
-            <?php $img_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-                style="background-image: url('<?php echo($img_url); ?>');"
-            <?php else : ?>
-                <?php if ( get_header_image() ) : ?>
-                    style="background-image: url('<?php header_image(); ?>');"
-                <?php endif; ?>
-            <?php endif; ?>
-        >
-            <div class="sans-background-tint">
-                <header id="masthead" role="banner">
-                    <?php get_template_part( '/template-parts/header', 'nav' ); ?>
-                    <div class="sans-top-brand">
-                        <div class="sans-cont">
-                                <?php
-                                    if ( function_exists( 'the_custom_logo' ) ) {
-                                        the_custom_logo();
-                                    }
-                                ?>
-                        </div>
-                    </div>
-                </header>
-                <div class="arch-small" style="background-image:url(<?php echo(get_stylesheet_directory_uri()); ?>/img/arch-4.svg);">
-                </div>
-            </div>
-        </div>
-        
-        <div class="sans-pg-content">
+    <?php get_template_part( '/custom/header', 'main' ); ?>
 
